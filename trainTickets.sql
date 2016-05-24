df974fad454f1bf76f4c664d4248d4be6f99fe47
v613 <cebandima@ymail.com>
2016-05-24 22:09:41 +0300

    initial database with few records

---
+++ trainTickets.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 24, 2016 at 09:08 
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `trainTickets`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(3) NOT NULL,
+  `paswd` varchar(30) NOT NULL,
+  `login` varchar(30) NOT NULL,
+  `last-date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `paswd`, `login`, `last-date`) VALUES
+(1, 'petros-naghibatorXXXL', 'petru', '2016-05-24 08:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE `city` (
+  `name` varchar(20) NOT NULL,
+  `distance` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`name`, `distance`) VALUES
+('Anenii Noi', 35),
+('Balti', 135),
+('Basarabeasca', 95),
+('Bender', 60),
+('Briceni', 239),
+('Cahul', 168),
+('Calarasi', 53),
+('Cantemir', 121),
+('Causeni', 70),
+('Chisinau', 0),
+('Cimislia', 68),
+('Comrat', 99),
+('Donduseni', 215),
+('Drochia', 190),
+('Falesti', 138),
+('Floresti', 125),
+('Glodeni', 167),
+('Ocnita', 242),
+('Rezina', 99),
+('Ribnita', 105),
+('Soldanesti', 108),
+('Straseni', 27),
+('Taraclia', 69),
+('Tiraspol', 70),
+('Ugheni', 110);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `first-name` varchar(15) NOT NULL,
+  `second-name` varchar(15) NOT NULL,
+  `id-order` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `first-name`, `second-name`, `id-order`) VALUES
+(1, 'FOCSA', 'PETRU', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(10) NOT NULL,
+  `start` varchar(20) NOT NULL,
+  `destination` varchar(20) NOT NULL,
+  `price` int(4) NOT NULL,
+  `id-train` int(3) NOT NULL,
+  `start-time` datetime NOT NULL,
+  `destination-time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `start`, `destination`, `price`, `id-train`, `start-time`, `destination-time`) VALUES
+(1, 'Chisinau', 'Causeni', 150, 3, '2016-05-23 12:25:00', '2016-05-24 13:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `train`
+--
+
+CREATE TABLE `train` (
+  `id` int(2) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `place` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `train`
+--
+
+INSERT INTO `train` (`id`, `type`, `name`, `place`) VALUES
+(1, 'pasager', 'KRMI-P', 300),
+(2, 'pasager', 'LSAKFJ-P', 600),
+(3, 'pasager', 'ASD-P', 350),
+(4, 'marfa', 'FS48', 0),
+(5, 'marfa', 'FS52', 0),
+(6, 'pasager', 'KJK-P', 400);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `types`
+--
+
+CREATE TABLE `types` (
+  `type` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `types`
+--
+
+INSERT INTO `types` (`type`) VALUES
+('marfa'),
+('pasager');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `train`
+--
+ALTER TABLE `train`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `types`
+--
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`type`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `train`
+--
+ALTER TABLE `train`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
